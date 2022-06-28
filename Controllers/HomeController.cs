cd155b069b9f39fa56b96cca1ad4cd9616590b61
MahaAmershk <100950001+MahaAmershk@users.noreply.github.com>
2022-06-29 00:46:16 +0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_19101001_002.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
