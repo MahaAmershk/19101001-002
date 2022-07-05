e2a84692af67c133e80f7b214ab55ab858aa348c
MahaAmershk <100950001+MahaAmershk@users.noreply.github.com>
2022-07-05 20:18:05 +0500

    Changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,13 +8,20 @@ namespace Project_19101001_002.Controllers
         {
             return View();
         }
-        public IActionResult showdata(string firstname,string secondname,string gender,string s)
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        public IActionResult showdata(string firstname,string secondname,string email,string phn,string time, string dropdown)
         {
             ViewBag.MessageF = "First-Name:" + firstname;
             ViewBag.MessageS = "Second-Name:" + secondname;
-            ViewBag.MessageG = "Gender"+gender;
-            ViewBag.MessageD = "Deparrment:" + s;
-            
+            ViewBag.MessageG = "Email"+email;
+            ViewBag.MessageD = "Phone Number:" + phn;
+            ViewBag.MessageT = "Time:" + time;
+            ViewBag.MessageSe = "Service selected:" + dropdown;
+
+
             return View();
         }
     }
